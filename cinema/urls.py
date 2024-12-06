cb7f176fe96eb612e81ba8c3055df04047e46887
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-12-06 16:32:48 +0200

    created endpoints

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,17 @@
 # write urls here
+from os.path import pathsep
+
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import GenreViewSet, ActorViewSet, CinemaHallViewSet, MovieViewSet, MovieSessionViewSet
+
+router = DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("cinema-halls", CinemaHallViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie-sessions", MovieSessionViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
